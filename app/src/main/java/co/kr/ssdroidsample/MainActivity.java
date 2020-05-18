23100a3e55c8ea4359ca8b6a4fcfe3bfb7dfa756
SongHoHak <ondraw@gmail.com>
2020-05-18 17:27:39 +0900

    Source cleanup

--- app/src/main/java/co/kr/ssdroidsample/MainActivity.java
+++ app/src/main/java/co/kr/ssdroidsample/MainActivity.java
@@ -38,32 +38,35 @@ public class MainActivity extends AppCompatActivity {
     Context mContext;
     ArrayList<SampleData> mLstSampleData = new ArrayList<SampleData>();
 
+    public void AddData()
+    {
+        mLstSampleData.add(new SampleData(ID_ViewPager,"SViewPager","This is a sample to switch page."));
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = this;
-        mLstSampleData.add(new SampleData(ID_ViewPager,"SViewPager","화면전환에 관련한 샘플입니다."));
+        AddData();
+        SetListView();
+    }
 
-
-        //SViewPager viewpager =  findViewById(R.id.vwPage);
-        //viewpager.StartPage(this,"Main",new Fragment1(),null);
-
+    void SetListView()
+    {
         ListView view = findViewById(R.id.lstSample);
-
         view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 SampleData data = mLstSampleData.get(i);
                 if(data.ID == ID_ViewPager)
                 {
-                    Intent intent=new Intent(MainActivity.this, SViewPagerActivity.class);
+                    Intent intent = new Intent(MainActivity.this, SViewPagerActivity.class);
                     startActivity(intent);
                 }
             }
         });
-
-
         view.setAdapter(new BaseAdapter() {
             @Override
             public int getCount() { return mLstSampleData.size(); }
@@ -73,8 +76,7 @@ public class MainActivity extends AppCompatActivity {
             public long getItemId(int i) { return 0; }
             @Override
             public View getView(int i, View view, ViewGroup viewGroup) {
-                if(view == null)
-                    view = ((LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.row_samples,viewGroup,false);
+                if(view == null)  view = ((LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.row_samples,viewGroup,false);
                 SampleData data = mLstSampleData.get(i);
                 TextView title = view.findViewById(R.id.txtTitle);
                 title.setText(data.Title);
@@ -83,7 +85,5 @@ public class MainActivity extends AppCompatActivity {
                 return view;
             }
         });
-
-
     }
 }
